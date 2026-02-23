1b4b3b6c2c60d59a1a31f3909adf14e333a5e3af
Naman Choudhary <n.choudhary-1@student.tudelft.nl>
2026-02-23 17:19:33 +0100

    Implementing the show instance and inserting the encodeUnicode function (from the patch)

--- src/Jq/Json.hs
+++ src/Jq/Json.hs
@@ -1,21 +1,49 @@
 module Jq.Json where
 
+import Text.Printf (printf)
+import Data.Char (isControl, ord)
+
 data JSON =
      JNull
   |  JNumber Double
   |  JString String     
   |  JBool Bool
   |  JArray [JSON]
-  |  Jobject [(String, JSON)] 
+  |  JObject [(String, JSON)] 
 
 instance Show JSON where
-  show JNull   = "null"
-  show _       = undefined
+  show JNull = "null"
+  show (JNumber num) = show num
+  show (JString string) = '"': concatMap encodeUnicode string ++ "\""
+  show (JBool True) = "true"
+  show (JBool False) = "false"
+  show (JArray array) = "[" ++ formatInput (map show array) ++ "]"
+  show (JObject object) = "{" ++ formatInput (map showKeyValuePair object) ++ "}"
+    where
+      showKeyValuePair (key, value) = show (JString key) ++ ":" ++ show value  
+
+formatInput :: [String] -> String
+formatInput [] = ""
+formatInput [s] = s
+formatInput (s:ss) = s ++ "," ++ formatInput ss
 
 instance Eq JSON where
   JNull == JNull = True
   _ == _ = undefined
 
+encodeUnicode :: Char -> String
+encodeUnicode '\b' = "\\b"
+encodeUnicode '\f' = "\\f"
+encodeUnicode '\n' = "\\n"
+encodeUnicode '\r' = "\\r"
+encodeUnicode '\t' = "\\t"
+encodeUnicode '"' = "\\\""
+encodeUnicode '\\' = "\\\\"
+encodeUnicode c
+  | ord c >= 0x80 && ord c <= 0x9f = [c]
+  | isControl c = printf "\\u%04x" (ord c)
+  | otherwise = [c]
+  
 -- Smart constructors
 -- These are included for test purposes and aren't meant to correspond one to one
 -- with the actual constructors of the JSON datatype.
