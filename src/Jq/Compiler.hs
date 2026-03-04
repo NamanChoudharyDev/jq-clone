1c65078f2b658f6ae58da1b3c6216fc3be8d19c7
Naman Choudhary <n.choudhary-1@student.tudelft.nl>
2026-03-04 18:50:36 +0100

    Implement case for compiling StringIndexing

--- src/Jq/Compiler.hs
+++ src/Jq/Compiler.hs
@@ -2,12 +2,27 @@ module Jq.Compiler where
 
 import           Jq.Filters
 import           Jq.Json
+import Data.Foldable (find)
 
 
 type JProgram a = JSON -> Either String a
 
 compile :: Filter -> JProgram [JSON]
 compile (Identity) inp = return [inp]
+
+compile (StringIndexing key) JNull = return [Jnull]
+compile (StringIndexing key) (JObject inp) = 
+    case findValueAssociatedToKey key inp of
+        Just value -> return [value]
+        Nothing -> return [Jnull]
+compile (StringIndexing key) _ = Left "You passed a Json type that is not indexebl"
+
+
+findValueAssociatedToKey :: Eq a => a -> [(a, b)] -> Maybe b
+findValueAssociatedToKey _ [] = Nothing
+findValueAssociatedToKey key ((k, v):xs)
+    | key == k = Just v
+    | otherwise = findValueAssociatedToKey key xs
 
 run :: JProgram [JSON] -> JSON -> Either String [JSON]
 run p j = p j
