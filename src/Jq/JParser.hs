9c2247858bffa9916098d21b38470780f8145b48
Naman Choudhary <n.choudhary-1@student.tudelft.nl>
2026-03-18 18:03:14 +0100

    Implement parser for JArray in Jparser.hs

--- src/Jq/JParser.hs
+++ src/Jq/JParser.hs
@@ -16,5 +16,22 @@ parseJNumber = JNumber . read <$> rawNumber
 parseJBool :: Parser JSON
 parseJBool = (JBool True <$ string "true") <|> (JBool False <$ string "false")
 
+parseJArray :: Parser JSON
+parseJArray = parseEmptyArray <|> parseNonEmptyArray
+
+parseEmptyArray :: Parser JSON
+parseEmptyArray = do
+  _ <- token (char '[')
+  _ <- token (char ']')
+  return (JArray [])
+
+parseNonEmptyArray :: Parser JSON
+parseNonEmptyArray = do
+  _ <- token (char '[')
+  x <- parseJSON
+  xs <- many (token (char ',') *> parseJSON)
+  _ <- token (char ']')
+  return (JArray (x : xs))
+
 parseJSON :: Parser JSON
-parseJSON = token $ parseJNull <|> parseJNumber <|> parseJString <|> parseJBool
+parseJSON = token $ parseJNull <|> parseJNumber <|> parseJString <|> parseJBool <|> parseJArray
